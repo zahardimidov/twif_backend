3d7f020387cd40280f65ef2aff80e4c6b22408c5
Захар Димидов <zahardimidov@MacBook-Air-Zahar.local>
2024-10-21 22:43:58 +0500

    commit

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ TEST_MODE = True
 TEST_USER_ID = 7485502073
 
 PORT = 4550
-WEBAPP_URL = os.environ.get('WEBAPP_URL', 'https://192.168.0.8:5173')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '5166691505:AAEbw80sx5EGT7pB9eT954PTO3WtowUMEME')
-WEBHOOK_HOST = os.environ.get('WEBHOOK_HOST', 'https://d8cd-2a00-1fa2-120-bf9b-61ef-3d33-292e-1d69.ngrok-free.app')
+WEBAPP_URL = os.environ.get('WEBAPP_URL', 'https://twif8-feb8a.web.app')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '7396897324:AAHzNa_ncI4sf0hkg8M0txRt9KWZSnXFqC0')
+WEBHOOK_HOST = os.environ.get('WEBHOOK_HOST', 'https://playcloud.pro')
 WEBHOOK_PATH = 'webhook'
 #ENGINE = "sqlite+aiosqlite:///./database/database.db"
 ADMIN_USERNAME = 'admin'
